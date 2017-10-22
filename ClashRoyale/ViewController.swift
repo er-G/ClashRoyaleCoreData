006d7c4fc8fbea504a695f39f9038662d31a7c53
Jorge MR <jorge_mr@icloud.com>
2017-10-22 13:29:01 -0500

    Añade las imágenes de los botones

--- ClashRoyale/ViewController.swift
+++ ClashRoyale/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //button.titleLabel?.numberOfLines = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func Agregar(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buscar(_ sender: UIButton) {
+    }
+    
+    @IBAction func Coleccion(_ sender: UIButton) {
+    }
+    
+    @IBAction func Borrar(_ sender: UIButton) {
+    }
+    
 }
 
