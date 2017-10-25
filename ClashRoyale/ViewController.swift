2bc734d1af4d410e4d7d2f1967720c223762ff88
Jorge A Mtz Rojas <31960313+jorge-mr@users.noreply.github.com>
2017-10-25 15:39:41 -0500

    creamos el contexto

--- ClashRoyale/ViewController.swift
+++ ClashRoyale/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -14,8 +15,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var nombre: UILabel!
     @IBOutlet weak var descripcion: UILabel!
     
+    var managedContext : NSManagedObjectContext!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         // Do any additional setup after loading the view, typically from a nib.
         //button.titleLabel?.numberOfLines = 0
     }
@@ -45,7 +49,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func crearPersonaje(imagen : UIImage){
+        let alertController = UIAlertController(title: "Nuevo personaje", message: "Ingresa sus dfatos", preferredStyle: .alert)
+        alertController.addTextField { (textField : UITextField) in
+            textField.placeholder = "Nombre ..."
+        }
+        alertController.addTextField { (textField: UITextField) in
+            textField.placeholder = "Descripcion ..."
+        }
+        let alertAction = UIAlertAction(title: "Guardar", style: .default) { (action: UIAlertAction) in
+            let nombreTextField = alertController.textFields?[0]
+            let descripcionTextField = alertController.textFields?[1]
+            
+            if nombreTextField?.text != "" && descripcionTextField?.text != "" {
+                self.nombre.text = nombreTextField?.text
+                self.descripcion.text = descripcionTextField?.text
+                self.imagen.image = imagen
+                
+                //CORE DATA INSERT
+                
+                
+            }
+        }
         
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func Buscar(_ sender: UIButton) {
